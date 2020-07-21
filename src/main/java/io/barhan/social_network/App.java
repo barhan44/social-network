536383ba97e52c771acb98257e77db2c88dac516
barhan44 <gunyashov44@gmail.com>
2020-07-21 23:42:19 +0300

    init commit + maven config

---
+++ src/main/java/io/barhan/social_network/App.java
@@ -0,0 +1,13 @@
+package io.barhan.social_network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
