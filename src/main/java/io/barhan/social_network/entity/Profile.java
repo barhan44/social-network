47520673d2c71941650f68ddc46418479cb164c4
barhan44 <gunyashov44@gmail.com>
2020-08-08 21:33:44 +0300

    Squashed commit of the following:
    commit 921811e6530eecdc1b184e1e7ce2abaf77786587
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Sat Aug 8 21:32:37 2020 +0300
    
        Add: skill category entity
    
    commit a8f49dd0b4424fb9fa4da8ab50a828e040aa7a2b
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Sat Aug 8 21:29:10 2020 +0300
    
        Add: skill entity
    
    commit b79fa5c2b3c75adfbbff27e5a5cf827b1b6392bc
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Sat Aug 8 21:17:43 2020 +0300
    
        Add: education entity
    
    commit cc8e8707c4f729b12bdfb63e041c992b826c039d
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Sat Aug 8 21:05:53 2020 +0300
    
        Add: hobby entity
    
    commit 3e4856676e394cf817b9e7636e691f9ae7c627e0
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Thu Aug 6 22:18:33 2020 +0300
    
        Add: main profile tag implementation
    
    commit 4b096e52fe99c11405dd7ecb7c1cd46b52d94023
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Thu Aug 6 22:18:16 2020 +0300
    
        Add: font awesome fonts
    
    commit b8dcb7710addff0f9c0affe9fee69ac729f912d2
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Thu Aug 6 22:00:03 2020 +0300
    
        Add: profile entity

--- src/main/java/io/barhan/social_network/entity/Profile.java
+++ src/main/java/io/barhan/social_network/entity/Profile.java
@@ -1,6 +1,8 @@
 package io.barhan.social_network.entity;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,10 +22,10 @@ public class Profile extends AbstractEntity<Long> implements Serializable {
 	@Id
 	@SequenceGenerator(name = "PROFILE_ID_GENERATOR", sequenceName = "PROFILE_SEQ", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PROFILE_ID_GENERATOR")
-	@Column(unique = true, nullable = false)
+	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
 
-	@Column(nullable = false, length = 100)
+	@Column(name = "uid", nullable = false, length = 100)
 	private String uid;
 
 	@Column(name = "first_name", nullable = false, length = 100)
@@ -31,6 +33,36 @@ public class Profile extends AbstractEntity<Long> implements Serializable {
 
 	@Column(name = "last_name", nullable = false, length = 100)
 	private String lastName;
+
+	@Column(name = "large_photo", length = 255)
+	private String largePhoto;
+
+	@Column(name = "small_photo", length = 255)
+	private String smallPhoto;
+
+	@Column(name = "phone", length = 20)
+	private String phone;
+
+	@Column(name = "email", length = 100)
+	private String email;
+
+	@Column(name = "city", length = 100)
+	private String city;
+
+	@Column(name = "country", length = 60)
+	private String country;
+
+	@Column(name = "birth_day")
+	private Date birthDay;
+
+	@Column(name = "password", length = 100, nullable = false)
+	private String password;
+
+	@Column(name = "completed", nullable = false)
+	private boolean completed;
+
+	@Column(name = "created", insertable = false)
+	private Timestamp created;
 
 	public Profile() {
 	}
@@ -51,13 +83,101 @@ public class Profile extends AbstractEntity<Long> implements Serializable {
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
-	
+
 	public String getFirstName() {
 		return this.firstName;
 	}
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLargePhoto() {
+		return largePhoto;
+	}
+
+	public void setLargePhoto(String largePhoto) {
+		this.largePhoto = largePhoto;
+	}
+
+	public String getSmallPhoto() {
+		return smallPhoto;
+	}
+
+	public void setSmallPhoto(String smallPhoto) {
+		this.smallPhoto = smallPhoto;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public Timestamp getCreated() {
+		return created;
+	}
+
+	public void setCreated(Timestamp created) {
+		this.created = created;
 	}
 
 	@Transient
--- src/main/webapp/WEB-INF/tags/profile-main.tag
+++ src/main/webapp/WEB-INF/tags/profile-main.tag
@@ -1,7 +1,16 @@
-<%@ tag pageEncoding="UTF-8" trimDirectiveWhitespaces="true" %>
+<%@ tag pageEncoding="UTF-8" trimDirectiveWhitespaces="true"%>
 
 <div class="card">
-    <img class="card-img-top" src="https://image.flaticon.com/icons/svg/3189/3189506.svg"
-         alt="profile-photo"/>
-    <h1 class="text-center">${profile.fullName}</h1>
+	<img class="card-img-top"
+		src="https://image.flaticon.com/icons/svg/3189/3189506.svg"
+		alt="profile-photo" />
+	<div class="card-body">
+		<h1 class="text-center card-title">${profile.fullName}</h1>
+		<h6 class="text-center card-subtitle">${profile.city},${profile.country}</h6>
+		<p class="text-center card-text">${profile.birthDay}</p>
+	</div>
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item"><i class="fa fa-phone"></i> ${profile.phone}</li>
+		<li class="list-group-item"><i class="fa fa-envelope"></i> ${profile.email}</li>
+	</ul>
 </div>
